270e3a21424cb68ac5d663094969741d4b389fe0
void-spark <81029971+void-spark@users.noreply.github.com>
2021-03-29 22:12:00 +0200

    Add more comments to read part of uart

--- rtl/simpleuart.v
+++ rtl/simpleuart.v
@@ -42,13 +42,14 @@ module simpleuart #(parameter integer DEFAULT_DIV = 1) (
 	/// Serial value register
 	// Write enable for the register word, should stay high until reg_dat_wait is set low.
 	input         reg_dat_we,
-	// Read enable for the register word, should stay high until reg_dat_wait is set low.
+	// Read enable for the register word, the current implementation keeps reg_dat_wait low, and requires reg_dat_do to be read the same clock reg_dat_we is set high.
+	// The clock after the clock reg_dat_re is set high the value of reg_dat_do will be reset.
 	input         reg_dat_re,
 	// The register word to be written to externally, must stay set until reg_dat_wait is set low.
 	// Only the lowest 8 bits are actually used.
 	input  [31:0] reg_dat_di,
-	// The register word to be read externally, must wait until reg_dat_wait is set low to be read.
-	// Only the lowest 8 bits are actually driven.
+	// The register word to be read externally, the current implementation requires reg_dat_do to be read the same clock reg_dat_we is set high.
+	// If a byte was received it's value is made available in the lowest 8 bits, otherwise all 32 bits are set to 1 (= -1).
 	output [31:0] reg_dat_do,
 	// Bit which indicates the serial register word read or write is not done yet.
 	output        reg_dat_wait
@@ -59,9 +60,13 @@ module simpleuart #(parameter integer DEFAULT_DIV = 1) (
 
 	// Receive state machine state.
 	reg [3:0] recv_state;
+	// Starts at 0 on first pos-edge after reset, and counts up 1 each following pos-edge.
+	// Counts up to the set divider + 1 for each bit, which means each bit is div+2 clocks long (0 .. div+1)
+	// TODO: Apply https://github.com/cliffordwolf/picorv32/pull/162/files
 	reg [31:0] recv_divcnt;
 	reg [7:0] recv_pattern;
 	reg [7:0] recv_buf_data;
+	// Bit which indicates if a new byte is available in the receive buffer.
 	reg recv_buf_valid;
 
 	// Buffer with pattern to send, rightmost bit is attached to ser_tx
@@ -70,7 +75,7 @@ module simpleuart #(parameter integer DEFAULT_DIV = 1) (
 	// Bits left to send
 	reg [3:0] send_bitcnt;
 	// Starts at 0 on first pos-edge after reset, and counts up 1 each following pos-edge.
-	// Counts up to the set divider + 1 for each bit, which means each bit is dev+2 clocks long (0 .. dev+1)
+	// Counts up to the set divider + 1 for each bit, which means each bit is div+2 clocks long (0 .. div+1)
 	// TODO: Apply https://github.com/cliffordwolf/picorv32/pull/162/files
 	reg [31:0] send_divcnt;
 	// Bit to indicate sending should be initialized, we keep tx high for 16 bit lengths.
@@ -80,9 +85,10 @@ module simpleuart #(parameter integer DEFAULT_DIV = 1) (
 	assign reg_div_do = cfg_divider;
 
 	// Once the external system has set a byte to send, make it wait till it's send.
+	// Note that it stays low for a read.
 	assign reg_dat_wait = reg_dat_we && (send_init || send_bitcnt);
 
-	// Reading the serial value register gives the current received byte, or 255/-1 if not valid.
+	// Reading the serial value register gives the current received byte, or all 32 bits high/-1 if not valid.
 	assign reg_dat_do = recv_buf_valid ? recv_buf_data : ~0;
 
 	// Control setting the cfg_divider value:
@@ -102,38 +108,58 @@ module simpleuart #(parameter integer DEFAULT_DIV = 1) (
 	// Receiving state machine
 	always @(posedge clk) begin
 		if (!resetn) begin
+			// Set initial values
 			recv_state <= 0;
 			recv_divcnt <= 0;
 			recv_pattern <= 0;
 			recv_buf_data <= 0;
+			// We start with nothing in the receive buffer, so mark it as not valid.
 			recv_buf_valid <= 0;
 		end else begin
+			// Always count up the divider count when we're not in reset.
 			recv_divcnt <= recv_divcnt + 1;
 			if (reg_dat_re)
+				// When a byte is requested, mark the receive buffer as invalid, which means we return -1 on read.
+				// Reading must happen this clock, which will return a value only if the receive buffer value is set.
 				recv_buf_valid <= 0;
 			case (recv_state)
 				0: begin
+					// Initial/idle state, wait for start bit (rx low)
 					if (!ser_rx)
+						// Go to the next state once rx goes low (begin of start bit)
 						recv_state <= 1;
+					// Keep the divider count at 0 while we're waiting for the start bit.
 					recv_divcnt <= 0;
 				end
 				1: begin
+					// Wait for half the divider value clocks. TODO: Check exact count.
+					// This should put us halfway the start bit.
 					if (2*recv_divcnt > cfg_divider) begin
+						// Go to the next state (read first bit value)
 						recv_state <= 2;
+						// Reset divider counter
 						recv_divcnt <= 0;
 					end
 				end
 				10: begin
+					// Wait for one bit of clocks, which puts us halfway the last/stop bit.
 					if (recv_divcnt > cfg_divider) begin
+						// Copy the received pattern
 						recv_buf_data <= recv_pattern;
+						// Mark the receive buffer as valid.
 						recv_buf_valid <= 1;
+						// Reset the state machine to the initial state, ready for the next start bit.
 						recv_state <= 0;
 					end
 				end
 				default: begin
+					// Wait for one bit of clocks, which puts us halfway the next bit.
 					if (recv_divcnt > cfg_divider) begin
+						// Store the value on rx
 						recv_pattern <= {ser_rx, recv_pattern[7:1]};
+						// Got to the next state (next bit, might be stop bit)
 						recv_state <= recv_state + 1;
+						// Reset divider counter
 						recv_divcnt <= 0;
 					end
 				end
